c91b1799cbd44545fb1eb3b0cabf636d8cd53133
Madeline Lim <deltabouche@gmail.com>
2018-05-19 21:34:07 -0700

    Support glob

--- map2xml.rb
+++ map2xml.rb
@@ -3,7 +3,7 @@ require 'ruby2d'
 
 # fn = 'app/Content/Maps/1-ForsakenCity.bin'
 
-fn = ARGV[0]
-
-a = CelesteMapReader.new(fn)
-File.open("#{File.basename(fn)}.xml", "wb") { |f| f.write a.root.inspect }
+ARGV.each do |fn|
+  a = CelesteMapReader.new(fn)
+  File.open("#{File.basename(fn)}.xml", "wb") { |f| f.write a.root.inspect }
+end
